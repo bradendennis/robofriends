4b262740a060eb26c3c72b3c090d07d9fa5bb593
bradendennis <59400789+bradendennis@users.noreply.github.com>
2020-02-14 14:02:15 -0500

    fixed scroll height

--- src/Components/Scroll.js
+++ src/Components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', height: '500px'}}>
+        <div style={{overflowY: 'scroll', height: '1000px'}}>
             {props.children}
         </div>
     )
