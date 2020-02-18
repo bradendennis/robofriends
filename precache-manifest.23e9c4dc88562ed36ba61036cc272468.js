d53a18fe7dc46c4a5b39acd103bbb48f5fea0dbe
bradendennis <59400789+bradendennis@users.noreply.github.com>
2020-02-18 16:35:29 -0500

    Updates

---
+++ precache-manifest.23e9c4dc88562ed36ba61036cc272468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1649eb51887bf1a8ee15f6bf7a07bed4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9fb42fd80bd441e60e13",
+    "url": "/robofriends/static/css/main.96d155c4.chunk.css"
+  },
+  {
+    "revision": "4ea886189846f3c75328",
+    "url": "/robofriends/static/js/2.37296ba2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.37296ba2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fb42fd80bd441e60e13",
+    "url": "/robofriends/static/js/main.9592c720.chunk.js"
+  },
+  {
+    "revision": "d6ceab57e5523008bfa1",
+    "url": "/robofriends/static/js/runtime-main.03e2f00f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofri/precache-manifest.95c9f3832b0f4a2573f1852cb84055e4.js"
+  "/robofriends/precache-manifest.23e9c4dc88562ed36ba61036cc272468.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofri/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
